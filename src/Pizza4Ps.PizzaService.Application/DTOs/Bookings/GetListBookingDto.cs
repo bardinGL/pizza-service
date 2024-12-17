d1cdfbd785c23628753e8ebe3bc0e17b71289d77
bardinGL <110034163+bardinGL@users.noreply.github.com>
2024-12-17 16:22:54 +0700

    Update minor error of declaring Id field in dtos

--- src/Pizza4Ps.PizzaService.Application/DTOs/Bookings/GetListBookingDto.cs
+++ src/Pizza4Ps.PizzaService.Application/DTOs/Bookings/GetListBookingDto.cs
@@ -9,7 +9,6 @@ namespace Pizza4Ps.PizzaService.Application.DTOs.Bookings
 {
     public class GetListBookingDto : PaginatedRequestDto
     {
-        public Guid? Id { get; set; }
         public DateTime? BookingDate { get; set; }
         public int? GuestCount { get; set; }
         public string? Status { get; set; }
--- src/Pizza4Ps.PizzaService.Application/DTOs/Categories/GetListCategoryDto.cs
+++ src/Pizza4Ps.PizzaService.Application/DTOs/Categories/GetListCategoryDto.cs
@@ -4,7 +4,6 @@ namespace Pizza4Ps.PizzaService.Application.DTOs.Categories
 {
     public class GetListCategoryDto : PaginatedRequestDto
     {
-        public Guid? Id { get; set; }
         public string? Name { get; set; }
         public string? Description { get; set; }
     }
